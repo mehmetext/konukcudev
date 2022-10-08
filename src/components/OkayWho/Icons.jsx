a84c4b978bee5a3872c4bf2a6fa93cac4e881e05
Mehmet Konukcu <mehmetext68@gmail.com>
2022-10-08 23:32:04 +0300

    OkayWho componenti eklendi

---
+++ src/components/OkayWho/Icons.jsx
@@ -0,0 +1,26 @@
+import { FaMobileAlt, FaGlobe, FaBrush } from "react-icons/fa";
+
+export default function Icons() {
+	return (
+		<div className="grid md:grid-cols-3 grid-cols-1 w-full">
+			<div className="okayWho-icon">
+				<FaMobileAlt className="mb-4 text-9xl" />
+				<div className="font-slab font-bold text-center text-2xl">
+					Mobil Uygulama Geliştirici
+				</div>
+			</div>
+			<div className="okayWho-icon">
+				<FaGlobe className="mb-4 text-9xl" />
+				<div className="font-slab font-bold text-center text-2xl">
+					Website Geliştirici
+				</div>
+			</div>
+			<div className="okayWho-icon">
+				<FaBrush className="mb-4 text-9xl" />
+				<div className="font-slab font-bold text-center text-2xl">
+					Grafik Tasarımcı
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/components/OkayWho/index.jsx
+++ src/components/OkayWho/index.jsx
@@ -1,11 +1,13 @@
 import MehmetKonukcu from "../MehmetKonukcu";
 import Section from "../Section";
+import Icons from "./Icons";
 
 export default function OkayWho() {
 	return (
 		<Section isLight={true}>
-			<div className="flex justify-center items-center">
+			<div className="flex justify-center items-center flex-col">
 				<MehmetKonukcu title={"Tamam da, kim bu?"} showLine={true} />
+				<Icons />
 			</div>
 		</Section>
 	);
