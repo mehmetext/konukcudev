1840c284236be7ecf7e80b62303df5293a8b5f0f
Mehmet Konukcu <mehmetext68@gmail.com>
2022-10-08 23:37:25 +0300

    public/images klasörü eklendi Veriler eklendi

---
+++ src/utils/data.js
@@ -0,0 +1,149 @@
+export const myExperiences = [
+	{
+		experienceGroupName: "Mobil Uygulama Geliştirici",
+		experiences: [
+			{
+				name: "DART",
+				rate: 85,
+			},
+			{
+				name: "FLUTTER",
+				rate: 85,
+			},
+		],
+	},
+	{
+		experienceGroupName: "Website Geliştirici",
+		experiences: [
+			{
+				name: "LARAVEL (RESTFUL API)",
+				rate: 50,
+			},
+			{
+				name: "JAVASCRIPT",
+				rate: 70,
+			},
+			{
+				name: "REACT",
+				rate: 65,
+			},
+			{
+				name: "REDUX",
+				rate: 65,
+			},
+			{
+				name: "TAILWIND.CSS",
+				rate: 70,
+			},
+		],
+	},
+	{
+		experienceGroupName: "Masaüstü Geliştirici",
+		experiences: [
+			{
+				name: "C#",
+				rate: 30,
+			},
+		],
+	},
+	{
+		experienceGroupName: "Grafik Tasarımcı",
+		experiences: [
+			{
+				name: "PHOTOSHOP",
+				rate: 85,
+			},
+			{
+				name: "ILLUSTRATOR",
+				rate: 65,
+			},
+			{
+				name: "ADOBE XD",
+				rate: 90,
+			},
+			{
+				name: "WEB TASARIM",
+				rate: 90,
+			},
+			{
+				name: "MOBİL UI TASARIMI",
+				rate: 80,
+			},
+			{
+				name: "KARTVİZİT TASARIMI",
+				rate: 85,
+			},
+		],
+	},
+	{
+		experienceGroupName: "Diğer",
+		experiences: [
+			{
+				name: "EXCEL",
+				rate: 85,
+			},
+			{
+				name: "İNGİLİZCE",
+				rate: 50,
+			},
+			{
+				name: "CAMTASIA STUDIO",
+				rate: 60,
+			},
+			{
+				name: "ÖĞRENME İSTEĞİ",
+				rate: 1000,
+			},
+		],
+	},
+];
+
+export const myReferences = [
+	{
+		referenceGroupName: "Mobil Uygulama Geliştirici",
+		references: [
+			{
+				image: "fotonot.jpg",
+				title: "FOTONOT",
+			},
+			{
+				image: "sprien.jpg",
+				title: "SPRIEN",
+			},
+			{
+				image: "calcubody.jpg",
+				title: "CALCUBODY",
+			},
+		],
+	},
+	{
+		referenceGroupName: "Web Geliştirici",
+		references: [
+			{
+				image: "kendine.jpg",
+				title: "KENDİNE TEMA (MYBB)",
+			},
+		],
+	},
+	{
+		referenceGroupName: "Grafik Tasarımcı",
+		references: [
+			{
+				image: "aytec.jpg",
+				title: "AY-TEC KARTVİZİT",
+			},
+			{
+				image: "kipost.png",
+				title: "Kİ POST TASARIMI",
+			},
+			{
+				image: "instagramstory.jpg",
+				title: "HİKAYE",
+			},
+			{
+				image: "wdpost.png",
+				title: "WEBIDEBBY POST",
+			},
+		],
+	},
+];
