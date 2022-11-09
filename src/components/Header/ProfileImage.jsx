7b463aa623819ffc43d619fb91e6ad6d1636a075
Mehmet Konukcu <mehmetext68@gmail.com>
2022-11-09 09:58:10 +0300

    ProfileImage hatası giderildi

--- src/components/Header/ProfileImage.jsx
+++ src/components/Header/ProfileImage.jsx
@@ -3,7 +3,7 @@ export default function ProfileImage() {
 		<div className="bg-white bg-opacity-5 rounded-full p-3 m-auto mb-3">
 			<img
 				className="rounded-full bg-white bg-opacity-5 p-3 w-[160px] h-[160px] sm:w-[260px] sm:h-[260px]"
-				src={process.env.PUBLIC_URL + "images/me.jpg"}
+				src={process.env.PUBLIC_URL + "/images/me.jpg"}
 				alt="Mehmet Konukçu"
 			/>
 		</div>
